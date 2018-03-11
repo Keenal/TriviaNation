97605adbfc734d1a73ae7ccb87e5224b70b6abd6
Randy <rcq1@students.uwf.edu>
2018-03-11 13:47:48 -0500

    Completed Trivia class testing

---
+++ TriviaNation/TriviaTest/TriviaTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TriviaNation
+{
+    [TestClass]
+    public class TriviaTest
+    {
+        private IQuestion question;
+        private IDataBaseTable database;
+        private ITrivia sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            question = new Questions();
+            database = new QuestionTable();
+            sut = null;
+        }
+
+        [TestMethod]
+        public void AquiringARandomIntegerShouldReturnNumbersBetweenOneAndTenInclusiveWhenTheNumberOfRowsInATableAreTen()
+        {
+            // Arrange
+            Mock<IDataBaseTable> mockDatabase = new Mock<IDataBaseTable>();
+            mockDatabase.Setup(r => r.RetrieveNumberOfRowsInTable()).Returns(10);
+            sut = new Trivia(mockDatabase.Object, question);
+            int i = 0;
+            Boolean flag;
+            
+            // Act
+            while (i < 1000)
+            {
+                int test = sut.RandomGenerator();
+
+                // Assert
+                Assert.AreNotEqual(0, test);
+                Assert.AreNotEqual(11, test);
+
+                if (test <= 0 || test >= 11)                
+                    Assert.Fail();
+                if (test >= 1 && test <= 10)
+                { 
+                    flag = true;
+                    Assert.IsTrue(flag);
+                }
+
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void AquiringARandomQuestionShouldReturnARandomTableRowFromDatabaseAndExtractOnlyTheQuestionFromTheRow()
+        {
+            // Arrange
+            Mock<IDataBaseTable> mockDatabase = new Mock<IDataBaseTable>();
+            mockDatabase.Setup(r => r.RetrieveNumberOfRowsInTable()).Returns(5);
+            mockDatabase.Setup(r => r.RetrieveTableRow(1)).Returns("This is the first row.\n And this is its answer");
+            mockDatabase.Setup(r => r.RetrieveTableRow(2)).Returns("This is the second row.\n And this is its answer");
+            mockDatabase.Setup(r => r.RetrieveTableRow(3)).Returns("This is the third row.\n And this is its answer");
+            mockDatabase.Setup(r => r.RetrieveTableRow(4)).Returns("This is the fourth row.\n And this is its answer");
+            mockDatabase.Setup(r => r.RetrieveTableRow(5)).Returns("This is the fifth row.\n And this is its answer");
+            sut = new Trivia(mockDatabase.Object, question);
+            int i = 0;
+
+            // Act
+            while (i <= 5)
+            {
+                string test = sut.GetRandomQuestion();
+                // Assert
+                if (test.Equals("This is the first row."))
+                    Assert.AreEqual("This is the first row.", test);
+                else if (test.Equals("This is the second row."))
+                    Assert.AreEqual("This is the second row.", test);
+                else if (test.Equals("This is the third row."))
+                    Assert.AreEqual("This is the third row.", test);
+                else if (test.Equals("This is the fourth row."))
+                    Assert.AreEqual("This is the fourth row.", test);
+                else if (test.Equals("This is the fifth row."))
+                    Assert.AreEqual("This is the fifth row.", test);
+                else
+                    Assert.Fail();
+                i++;
+            }
+
+        }
+
+        [TestMethod]
+        public void AnswerEvaluationShouldReturnTrueIfTheAnswerGivenMatchesTheActualAnswersStringValue()
+        {
+            // Arrange
+            string answerGiven = "False";
+            Mock<IQuestion> mockAnswer = new Mock<IQuestion>();
+            mockAnswer.Setup(r => r.Answer).Returns("False");
+            sut = new Trivia(database, mockAnswer.Object);
+
+            // Act
+            Boolean test = sut.EvaluateAnswer(answerGiven);
+
+            // Assert
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void AnswerEvaluationShouldReturnFalseIfTheAnswerGivenDoesNotMatchTheActualAnswersStringValue()
+        {
+            // Arrange
+            string answerGiven = "True";
+            Mock<IQuestion> mockAnswer = new Mock<IQuestion>();
+            mockAnswer.Setup(r => r.Answer).Returns("False");
+            sut = new Trivia(database, mockAnswer.Object);
+
+            // Act
+            Boolean test = sut.EvaluateAnswer(answerGiven);
+
+            // Assert
+            Assert.IsFalse(test);
+        }
+
+        [TestMethod]
+        public void AnswerEvaluationShouldMatchTheActualAnswerEvenIfTheAnswerGivenHasADifferentCaseOrHasExtraWhiteSpace()
+        {
+            // Arrange
+            string answerGiven = "  fAlSe  ";
+            Mock<IQuestion> mockAnswer = new Mock<IQuestion>();
+            mockAnswer.Setup(r => r.Answer).Returns("False");
+            sut = new Trivia(database, mockAnswer.Object);
+
+            // Act
+            Boolean test = sut.EvaluateAnswer(answerGiven);
+
+            // Assert
+            Assert.IsTrue(test);
+        }
+    }
+}
