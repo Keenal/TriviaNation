90318542140780e5bcf74121fae356b009f6a96e
Keenal <kms118@students.uwf.edu>
2018-03-13 01:58:49 -0500

    add tableExistsTest()

--- TriviaNation/TriviaNationTests/QuestionTableTest.cs
+++ TriviaNation/TriviaNationTests/QuestionTableTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TriviaNation;
 
 namespace TriviaNationTests
 {
@@ -9,16 +11,35 @@ namespace TriviaNationTests
 
         private String tableName;
         private String tableCreationString;
+        private QuestionTable sut;
 
         [TestInitialize]
         public void Initialize() {
             tableName = "QuestionTable";
             tableCreationString = "(question varchar(4000) not null PRIMARY KEY, answer varchar(4000) not null);";
+            sut = null;
         }
         [TestMethod]
         public void TestToSeeIfTableExists()
         {
+            
+            // Arrange
+            Mock<DataBaseOperations> mockDataBaseOperations = new Mock<DataBaseOperations>();
+            mockDataBaseOperations.Setup(r => r.TableExists).Returns(true);
+            sut = new QuestionTable();
 
+            // Act
+            bool expected = true;
+            bool actual = sut.TableExists(); 
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestToSeeIfTableIsCreated() {
+            
 
         }
     }
