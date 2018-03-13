95dee96d514958cf3184a221fd478af649b8b003
Keenal <kms118@students.uwf.edu>
2018-03-13 00:14:00 -0500

    add initialize() for QTTest

--- TriviaNation/TriviaNationTests/QuestionTableTest.cs
+++ TriviaNation/TriviaNationTests/QuestionTableTest.cs
@@ -6,9 +6,20 @@ namespace TriviaNationTests
     [TestClass]
     public class QuestionTableTest
     {
+
+        private String tableName;
+        private String tableCreationString;
+
+        [TestInitialize]
+        public void Initialize() {
+            tableName = "QuestionTable";
+            tableCreationString = "(question varchar(4000) not null PRIMARY KEY, answer varchar(4000) not null);";
+        }
         [TestMethod]
-        public void TestMethod1()
+        public void TestToSeeIfTableExists()
         {
+
+
         }
     }
 }
