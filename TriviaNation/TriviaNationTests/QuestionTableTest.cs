e4dfdcbca47a897bb052223c7422f423ba083ca7
Keenal <kms118@students.uwf.edu>
2018-03-13 04:26:17 -0500

    add test() in QuestionTableTest class

--- TriviaNation/TriviaNationTests/QuestionTableTest.cs
+++ TriviaNation/TriviaNationTests/QuestionTableTest.cs
@@ -8,17 +8,25 @@ namespace TriviaNationTests
     [TestClass]
     public class QuestionTableTest
     {
-        private IDataBaseTable sut;
         private string tableName;
+        private String tableCreationString;
+        private String insertString;
 
         [TestInitialize]
         public void Initialize() {
-            sut = null;
             tableName = "QuestionTable";
+            tableCreationString = "exTableCreationString";
+            insertString = "exInsertString";
         }
 
         [TestMethod]
-        public void TestToSeeIfATableIsCreated() {
+        public void TestToSeeIfATableExists() {
+            /* does not work with DBO
+             * 
+            // Arrange
+            Mock<DataBaseOperations> mockDatabase = new Mock<DataBaseOperations>();
+            mockDatabase.Setup(r => r.TableExists(tableName)).Returns(true);
+            */
 
             // Arrange
             Mock<IDataBaseTable> mockDatabase = new Mock<IDataBaseTable>();
@@ -32,5 +40,75 @@ namespace TriviaNationTests
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void TestToSeeIfATableIsCreated() {
+            // Arrange
+            Mock<IDataBaseTable> mockDatabase = new Mock<IDataBaseTable>();
+            mockDatabase.Setup(r => r.CreateTable());
+
+            // Act
+            void expected = "Creation of " + tableName + "complete!";
+            void actual = DataBaseOperations.CreateTable(tableName, tableCreationString);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestToSeeIfRowIsInserted() {
+            // Arrange
+            Mock<DataBaseOperations> dbo = new Mock<DataBaseOperations>();
+            dbo.Setup(r => r.InsertIntoTable(insertString)).Returns("Insertion complete!");
+
+            // Act
+            DataBaseOperations obj = new DataBaseOperations();
+
+            // Assert
+            Assert.AreEqual(obj.InsertIntoTable(dbo.Object), "Insertion complete!");
+            
+        }
+
+        [TestMethod]
+        public void TestToSeeIfNumOfRowsIsRetrieved()
+        {
+            // Arrange
+            Mock<DataBaseOperations> dbo = new Mock<DataBaseOperations>();
+            dbo.Setup(r => r.RetrieveNumberOfRowsInTable(tableName).Returns(3);
+
+            // Act
+            DataBaseOperations obj = new DataBaseOperations();
+
+            // Assert
+            Assert.AreEqual(obj.RetrieveNumberOfRowsInTable(tableName), 3);
+
+        }
+
+        [TestMethod]
+        public void TestToSeeIfRowIsRetrieved()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestToSeeIfNumOfColsIsRetrieved()
+        {
+            // Arrange
+            Mock<DataBaseOperations> dbo = new Mock<DataBaseOperations>();
+            dbo.Setup(r => r.RetrieveNumberOfColsInTable(tableName).Returns(2);
+
+            // Act
+            DataBaseOperations obj = new DataBaseOperations();
+
+            // Assert
+            Assert.AreEqual(obj.RetrieveNumberOfColsInTable(tableName), 2);
+        }
+
+        [TestMethod]
+        public void TestToSeeIfRowIsDeleted()
+        {
+
+        }
+
     }
 }
