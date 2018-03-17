84d20f82c16a5890d4f51acddea8bb188d1596a7
Wen <wx2@students.uwf.edu>
2018-03-17 11:07:26 -0500

    Add files via upload
    Encrypt the password

---
+++ CoverPassword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class CoverPassword : MonoBehaviour {
+
+	string passwordToEdit = string.Empty;
+
+	void OnGUI() {
+		passwordToEdit = GUI.PasswordField(new Rect(10, 10, 200, 20), passwordToEdit, "*"[0], 25);
+	}
+
+
+
+
+	void Update()
+	{
+		if (passwordToEdit == "password" && Input.GetKey ("enter")) {
+			Debug.Log ("log-in");
+		}
+	}
+
+}
