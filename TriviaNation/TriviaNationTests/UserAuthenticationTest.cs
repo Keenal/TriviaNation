43d971b65e83798146f781f9a3d41cc4890f0027
Randy <rcq1@students.uwf.edu>
2018-03-21 18:51:38 -0500

    Preparing for User unit tests

---
+++ TriviaNation/TriviaNationTests/UserAuthenticationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriviaNationTests
+{
+    [TestClass]
+    public class UserAuthenticationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
