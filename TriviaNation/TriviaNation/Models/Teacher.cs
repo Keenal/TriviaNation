0c26110c31e2338714d5e18f7a8f257ef9cb7bbf
Keenal <kms118@students.uwf.edu>
2018-04-17 14:50:39 -0500

    add teacher class in models

---
+++ TriviaNation/TriviaNation/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaNation.Models
+{
+    class Teacher
+    {
+        // game time limit
+        // question time limit
+        // select the question pack
+        // select a player 
+    }
+}
