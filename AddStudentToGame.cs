ac051bc1e15dd1d86d139a8bd606b3305cda38e2
Wen <wx2@students.uwf.edu>
2018-04-20 17:26:09 -0500

    Add files via upload

---
+++ AddStudentToGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TriviaNation;
+
+public class AddStudentToGame : MonoBehaviour {
+
+	public GameObject names;
+
+	private string Name;
+	/*
+	*read from Database and display all the student name on the screen
+	*/
+	void OnGUI() {
+
+		Name = "SAMMMMM";
+
+		GUI.Label(new Rect(200, 200, 300, 200), Name);
+
+		Name = names.tag;
+
+	}
+
+
+	public void addButton(){
+		
+	}
+
+
+	// Update is called once per frame
+	//void Update () {}
+}
