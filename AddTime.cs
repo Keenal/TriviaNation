395b75a968e7d36c4ff4372140787e568cbe8f49
Wen <wx2@students.uwf.edu>
2018-04-21 17:14:01 -0500

    Add files via upload

---
+++ AddTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TriviaNation;
+using UnityEngine.UI;
+using System.Linq;
+
+
+public class AddTime : MonoBehaviour {
+
+	public GameObject hour;
+	public GameObject minu;
+	public GameObject sec;
+
+
+	private string Hour;
+	private string Minu;
+	private string Sec;
+
+
+	public bool checkField()
+	{
+		if ((Hour != "") && (Minu != "") && (Sec != "")) {
+			return true;
+		}
+
+		return false;
+	}
+	public void SubmitButton()
+	{
+		if (checkField ()) {
+		
+			Debug.Log ("Set Time");
+		}
+	}
+}
