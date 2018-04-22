075d533313d7a5559e05d1cf73f5e8421d5df28b
Keenal <kms118@students.uwf.edu>
2018-04-22 02:40:55 -0500

    tests for quesionpack

--- TriviaNation/TriviaNationTests/QuestionPackTest.cs
+++ TriviaNation/TriviaNationTests/QuestionPackTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriviaNation;
 using TriviaNation.Models;
 
 namespace TriviaNationTests
@@ -31,6 +32,59 @@ namespace TriviaNationTests
             };
             // Assert
             Assert.AreEqual(121, pointValue.PointValue);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void SettingADatabaseShouldReturnDatabase()
+        {
+            // Arrange
+            QuestionPack database = new QuestionPack
+            {
+                // Act
+                Database = 134
+            };
+
+            // Assert
+            Assert.AreEqual(134, database.Database);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void AddingQuestion()
+        {
+            // Arrange
+            Mock<IQuestion> mockQuestion = new Mock<IQuestion>();
+            mockQuestion.Setup(r => r.Question).Returns("This is a test?");
+            //   sut = new TriviaAdministration(mockQuestion.Object, QT);
+
+            // Act
+            //   List<string> test = (List<String>)sut.GetValues();
+
+            // Assert
+            //      Assert.AreEqual("This is a test?", test[0]);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void MethodForDeletingAQuestionShouldFirstRetrieveARowFromDatabaseThroughPrivateMethodThenAccessOnlyTheQuestionColumnStringInOrderToEnterItAsTheArgumentForMethodDeleteRowFromTable()
+        {
+            // Arrange
+            string query = null;
+            Mock<IQuestion> mockDatabase = new Mock<IQuestion>();
+            mockDatabase.Setup(r => r.TableName).Returns("Table Name");
+            mockDatabase.Setup(r => r.RetrieveTableRow("Table Name", 1)).Returns("This is the question? \n This is the answer.\ntype");
+            mockDatabase.Setup(r => r.DeleteRowFromTable(It.IsAny<string>())).Callback<string>((s1) =>
+            {
+                query = s1;
+            });
+            //   sut = new TriviaAdministration(question, mockDatabase.Object);
+
+            // Act
+            //   sut.DeleteQuestion(1);
+
+            //Assert
+            Assert.AreEqual("This is the question? ", query);
         }
     }
 }
